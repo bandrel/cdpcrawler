a8707f86e38c4b5b89c501b33fe9eb95fd501577
bandrel <justin.bollinger@blackbox.com>
2016-12-15 13:58:14 -0500

    formating and extra code removed

--- cdpscanner.py
+++ cdpscanner.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 import os
+
 try:
     from netmiko import ConnectHandler
 except:
     print('[!] Looks like you are missing the paramiko library.  run \'pip install netmiko\'')
     if os.name == 'nt':
         print('[!] *NOTE* you must install the Microsoft Visual C++ Compiler for Python 2.7 ' \
-              'before installing netmiko.\r\n'\
+              'before installing netmiko.\r\n' \
               'This can be found at http://www.microsoft.com/en-us/download/details.aspx?id=44266')
 import getopt
 import sys
@@ -16,7 +17,8 @@ import getpass
 import socket
 from openpyxl import Workbook
 
-#help message
+
+# help message
 def helpmsg():
     print('Usage: cdpscanner.py [Options] <IP address or Host(optional)>' \
           '  Note: All options are optional.  User is prompted or defaults are used.' \
@@ -25,14 +27,12 @@ def helpmsg():
           '  -u or --username: specifies a username to use\n' \
           '  -p or --password: Specifies the password to use\n' \
           '  -c or --commands: Specifies a list of commands to send\n' \
-          '  -v or --verbose: Enables verbose output\n'\
-          '  -t or --telnet:  Enables fallback to telnet\n'\
-          '  -d or --directory: Specifies a a directory to place the output files into\n'\
-          '  --inventory:  Prints the inventory of all of the devices at the end\n'\
-          '  -o or --output:  Prints the inventory of all of the devices at the end\n'\
+          '  -v or --verbose: Enables verbose output\n' \
+          '  -t or --telnet:  Enables fallback to telnet\n' \
+          '  -d or --directory: Specifies a a directory to place the output files into\n' \
+          '  --inventory:  Prints the inventory of all of the devices at the end\n' \
+          '  -o or --output:  Prints the inventory of all of the devices at the end\n' \
           '  -H or --hosts:  specifies hosts via comma seperated values\n')
-
-
 
 
 def getinfo(username, password, host, commands, mode):
@@ -41,30 +41,30 @@ def getinfo(username, password, host, commands, mode):
     if mode == "SSH":
         device = {
             'device_type': 'cisco_ios',
-            'ip'         : host,
-            'username'   : username,
-            'password'   : password,
-            'port'       : 22,  # optional, defaults to 22
-            'secret'     : '',  # optional, defaults to ''
-            'verbose'    : False,  # optional, defaults to False
+            'ip': host,
+            'username': username,
+            'password': password,
+            'port': 22,  # optional, defaults to 22
+            'secret': '',  # optional, defaults to ''
+            'verbose': False,  # optional, defaults to False
         }
     elif mode == "Telnet":
         device = {
             'device_type': 'cisco_ios_telnet',
-            'ip'         : host,
-            'username'   : username,
-            'password'   : password,
-            'port'       : 23,  # optional, defaults to 22
-            'secret'     : '',  # optional, defaults to ''
-            'verbose'    : False,  # optional, defaults to False
+            'ip': host,
+            'username': username,
+            'password': password,
+            'port': 23,  # optional, defaults to 22
+            'secret': '',  # optional, defaults to ''
+            'verbose': False,  # optional, defaults to False
         }
-    print('Connecting to %s with %s' % (host,mode) )
+    print('Connecting to %s with %s' % (host, mode))
     # Create instance of SSHClient object
     net_connect = ConnectHandler(**device)
     # Automatically add untrusted hosts (make sure okay for security policy in your environment)
-    print('%s connection established to %s' % (mode,host))
+    print('%s connection established to %s' % (mode, host))
     # Use invoke_shell to establish an 'interactive session'
-    a = re.search(r'(.*)#',net_connect.find_prompt())
+    a = re.search(r'(.*)#', net_connect.find_prompt())
     if a is not None:
         hostname = a.group(1)
     else:
@@ -75,34 +75,38 @@ def getinfo(username, password, host, commands, mode):
         if command == 'show inventory':
             lines = output.split('\n\n')
             for line in lines:
-                new_line = line.replace('\n','')
-                a = re.search(r'NAME:\ ?(\S+).*PID:\ ?(\S+).*SN:\ ?(\S+)',new_line)
+                new_line = line.replace('\n', '')
+                a = re.search(r'NAME:\ ?(\S+).*PID:\ ?(\S+).*SN:\ ?(\S+)', new_line)
                 if a is not None:
                     pid = a.group(2)
                     serial = a.group(3)
-                    row.append([hostname,pid,serial])
+                    row.append([hostname, pid, serial])
         if verbose_mode:
             print(output)
     return all_output, row
+
 
 def find_hosts_from_output(output):
     neighbor_list = []
     global host_set
     global seen_before
-    cdp_regex = re.compile(r'\ *(\S+)(?:[^\r\n]*\r?\n){1,4}[\ ]*IP(?:v4)* [aA]ddress:\ (\S+)\r?\nPlatform: (?:cisco )*(\S+),[\ ]*Capabilities:.*(Switch)')
+    cdp_regex = re.compile(
+        r'\ *(\S+)(?:[^\r\n]*\r?\n){1,4}'
+        r'[\ ]*IP(?:v4)* [aA]ddress:\ (\S+)\r?\n'
+        r'Platform: (?:cisco )*(\S+),[\ ]*Capabilities:.*(Switch)')
     for item in output:
         split_output = item.split('Device ID:')
         for single_device in split_output:
-            matches = re.findall(cdp_regex,single_device)
+            matches = re.findall(cdp_regex, single_device)
             for match in matches:
                 if match[1] not in seen_before:
                     host_set.add(match[1])
                     seen_before.add(match[1])
-                neighbor_list.append([match[0],match[1],match[2]])
+                neighbor_list.append([match[0], match[1], match[2]])
     return neighbor_list
 
 
-#Declaration of global variables
+# Declaration of global variables
 inputfile = None
 host_set = set()
 username = ''
@@ -125,24 +129,22 @@ outputfile = 'output.xlsx'
 commands = ['show cdp neighbor detail',
             'show inventory']
 
-#setup excel workbook for output
+# setup excel workbook for output
 wb = Workbook()
 inventory_ws = wb.active
 inventory_ws.title = u'Inventory'
 neighbor_ws = wb.create_sheet("Neighbors")
 errors_ws = wb.create_sheet("Errors")
-#Add headers to worksheets
+# Add headers to worksheets
 inventory_ws.append(['Hostname', 'Device Model', 'Serial Number'])
 neighbor_ws.append(['Hostname', 'Neighbor Hostname', 'Neighbor IP', 'Neighbor Model'])
 errors_ws.append(['Hostname', 'Error', 'Protocol'])
 
-
-
 # Run CLI parser function to set variables altered from defaults by CLI arguments.
 try:
-    opts, args = getopt.getopt(sys.argv[1:],"i:u:p:hd:vtH:o:",
-                               ["input=", "user=", "password=","directory=","verbose","telnet",
-                                "inventory","hosts=","output="])
+    opts, args = getopt.getopt(sys.argv[1:], "i:u:p:hd:vtH:o:",
+                               ["input=", "user=", "password=", "directory=", "verbose", "telnet",
+                                "hosts=", "output="])
 except getopt.GetoptError:
     helpmsg()
     sys.exit(2)
@@ -152,7 +154,7 @@ for opt, arg in opts:
         sys.exit()
     elif opt in ('-i', '--input'):
         inputfile = arg
-        with open(org_dir+'/'+inputfile, 'rb') as hostfile:
+        with open(org_dir + '/' + inputfile, 'rb') as hostfile:
             for device in hostfile:
                 try:
                     socket.inet_aton(device.rstrip('\r\n'))
@@ -177,8 +179,6 @@ for opt, arg in opts:
     elif opt in ('-H', '--hosts'):
         for i in arg.split(','):
             host_set.add(i)
-    elif opt in ('--inventory'):
-        inventory_enabled = True
 for opt, arg in opts:
     if opt in ('-d', '--directory'):
         working_directory = arg
@@ -188,24 +188,14 @@ for opt, arg in opts:
             helpmsg()
             print(e)
             sys.exit()
-for arg in args:
-    try:
-        socket.inet_aton(arg)
-        host_set.add(arg)
-    except:
-        try:
-            ip_from_host = socket.gethostbyname(arg)
-            host_set.add(ip_from_host)
-        except:
-            print("%s is not a valid host name or IP address" % arg)
-            sys.exit(2)
-#Set list of IP addreses to connect to if the -i input file is not used
-if host_set == []:
+
+# Set list of IP addreses to connect to if the -i input file is not used
+if not host_set:
     host_set.append(raw_input("Enter Switch Hostname or IP Address: ").upper())
 if username == '':
     username = raw_input("Enter Username: ")
 if password == '':
-     password = getpass.getpass()
+    password = getpass.getpass()
 
 # Create a working copy of the set that contains all of the hosts.
 
@@ -213,20 +203,20 @@ if password == '':
 while host_set != set([]):
     # iterate through the set of hosts
     for host in current_set:
-            # remove the host you are going to connect to from the set.
+        # remove the host you are going to connect to from the set.
         currenthost = host_set.pop()
         device_output = ''
         try:
             # Try SSH and if that fails try telnet.
-            device_output, inventory_rows = getinfo(username, password, currenthost, commands,'SSH')
+            device_output, inventory_rows = getinfo(username, password, currenthost, commands, 'SSH')
             for row in inventory_rows:
                 inventory_ws.append(row)
-            # Check output for new hostnames
+                # Check output for new hostnames
         except Exception as e:
             print("SSH connection to %s failed" % host)
             print(e)
             failed_ssh.append(host)
-            errors_ws.append([host, unicode(e),'SSH'])
+            errors_ws.append([host, unicode(e), 'SSH'])
             if telnet_enabled:
                 try:
                     device_output, inventory_rows = getinfo(username, password, currenthost, commands, 'Telnet')
@@ -239,22 +229,12 @@ while host_set != set([]):
         finally:
             neighbor_list = find_hosts_from_output(device_output)
             for neighbor in neighbor_list:
-                neighbor_ws.append([host,neighbor[0],neighbor[1],neighbor[2]])
+                neighbor_ws.append([host, neighbor[0], neighbor[1], neighbor[2]])
 
     # Update the current list with the most recent updated host_set.
     current_set = set(host_set)
 
-#After everything has been completed or removed
-if inventory_enabled == True:
-    devices = glob.glob('*.txt')
-    for device in devices:
-        with open(device) as dfile:
-            for line in dfile:
-                matches = re.findall(r'PID:\ *(\S+),.*SN:(\S+)',line)
-                if matches != []:
-                    inventory.append([host,matches[0],matches[1]])
-
-
-for line in failed_hosts:
-    print('[!] %s failed both ssh and telnet' % line)
+for host in failed_ssh:
+    if host in failed_telnet:
+        print('[!] %s failed both ssh and telnet' % host)
 wb.save(outputfile)
